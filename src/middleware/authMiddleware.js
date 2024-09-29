797b98bf4572af8d5dde1e8d41ad4a98cd623066
Lynho <130713358+Julio-Cezar4@users.noreply.github.com>
2024-09-29 13:11:52 -0300

    Update authMiddleware.js

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,16 +1,17 @@
 import jwt from 'jsonwebtoken';
 
 export const authenticateToken = (req, res, next) => {
-    
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
 
     if (!token) {
+        console.warn('Tentativa de acesso sem token.');
         return res.status(401).json({ error: 'Token de autenticação não fornecido' });
     }
 
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
+            console.error('Token inválido ou expirado:', err);
             return res.status(403).json({ error: 'Token inválido ou expirado' });
         }
 
