1a5e19f26893ca5fd08cfa45093efd572255d930
Lynho <130713358+Julio-Cezar4@users.noreply.github.com>
2024-09-29 13:12:25 -0300

    Update authRotes.js

--- src/routes/authRotes.js
+++ src/routes/authRotes.js
@@ -1,19 +1,13 @@
 import express from 'express';
 import { login } from '../controllers/authController.js';
 import { authenticateToken } from '../middleware/authMiddleware.js';
+import { getUsers } from '../controllers/userController.js'; // Importa o controller para buscar usuários
 
 const router = express.Router();
 
 router.post('/login', login);
 
-router.get('/users', authenticateToken, async (req, res) => {
-    try {
-        const users = await prisma.user.findMany();
-        res.json(users); 
-    } catch (error) {
-        console.error('Erro ao buscar usuários:', error);
-        res.status(500).json({ error: 'Erro ao buscar usuários' });
-    }
-});
+// Rota para buscar usuários
+router.get('/users', authenticateToken, getUsers); // Usa o controller para buscar usuários
 
 export default router;
